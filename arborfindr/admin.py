8816b68f48474cbb6d9fc9a994409f04356e19fe
remoteconn-7891 <coreyjames689@aol.com>
2024-09-01 20:52:12 -0600

    new push with foreign key added

--- arborfindr/admin.py
+++ arborfindr/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from .models import Homeowner
 from .models import Arborist
 from .models import ArboristReview
+from .models import ServicesType
 
 # Register your models here.
 
 admin.site.register([Homeowner])
 admin.site.register([Arborist])
 admin.site.register([ArboristReview])
+admin.site.register([ServicesType])
